cf271b7e5110eaa7d41e3324119c22acfc873817
Samuel Toledo <samueltoledo.bmanagement@outlook.com>
2024-05-03 22:02:18 -0600

    Confirguracion intermedia

--- main.js
+++ main.js
@@ -1,2 +1,40 @@
+const employe = {
+    name: "Francisco",
+    lastName: "Tarrega",
+    age: 50
+}
 
-// PRUEBA JS
+const {age, name} =employe
+
+console.log(age);
+
+
+
+
+document.querySelectorAll(".control-container .clear-icon").forEach((element) => {
+    element.addEventListener("click", (e) => {
+        //Obtener el valor de una propiedad
+        const inputId = e.target.getAttribute("att-for");
+        
+        alert("Hola")
+    })
+})
+
+
+document.querySelectorAll(".control-container .input-search").forEach((element) => {
+    
+    element.addEventListener("keyup", (e) => {
+    
+        const {value} = e.target;
+    
+        if (!value) {
+            document.querySelector("#btn-load-more").disabled = true
+            return
+        }
+        document.querySelector("#btn-load-more").disabled = false
+    
+        console.log(value);
+    })
+})
+
+document.querySelector("#key-word")
